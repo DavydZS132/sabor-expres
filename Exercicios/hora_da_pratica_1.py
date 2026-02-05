c07ba882bf0e540de5b108d48af9340e465bd5e4
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-02-04 22:31:57 -0300

    Lidando com If, Elif e Else

---
+++ Exercicios/hora_da_pratica_1.py
@@ -0,0 +1,16 @@
+print('Python na Escola de Programação da Alura.\n')
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print("""A
+L
+U
+R
+A
+""")
+
+pi_arredondado = 3.14159
+
+print(f'O valor arredondado de pi é: {pi_arredondado:.2f}')
