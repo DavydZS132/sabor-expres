bdf5512ab6ef38b63790e099f07d44dcd18dc142
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-02-09 21:26:26 -0300

    Lidando com função

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-print("""
+import os
+
+def exibir_titulo():
+    print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█""")
 
-print('1.Cadastrar restaurante.')
-print('2.Listar restaurante.')
-print('3.Ativar restaurante.')
-print('4.Sair\n')
+def exibir_menu():
+    print('1.Cadastrar restaurante.')
+    print('2.Listar restaurante.')
+    print('3.Ativar restaurante.')
+    print('4.Sair\n')
 
-opcao_escolhida = int(input('Escolha a opção: '))
+def finalizando_app():
+    os.system('cls')
+    print('Finalizando App\n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-else:
-    print('Encerrando o programa')
+def exolher_opcao():
+
+    opcao_escolhida = int(input('Escolha a opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurantes')
+    else:
+        finalizando_app()
+
+def main():
+    exibir_titulo()
+    exibir_menu()
+    exolher_opcao()
+
+
+if __name__ == '__main__':
+    main()     
