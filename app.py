6493c954a9c3d376f654c6fb846937cb612e28ec
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-02-11 21:56:23 -0300

    Aprendendo TRY

--- app.py
+++ app.py
@@ -15,20 +15,29 @@ def finalizando_app():
     os.system('cls')
     print('Finalizando App\n')
 
+def opcao_invalida():
+    print('Opção invalida!\n')
+    input('Digite uma tecla para voltar ao menu: ')
+    main()
+
 def exolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha a opção: '))
 
-    opcao_escolhida = int(input('Escolha a opção: '))
-
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    else:
-        finalizando_app()
-
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizando_app()   
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 def main():
+    os.system('cls')
     exibir_titulo()
     exibir_menu()
     exolher_opcao()
