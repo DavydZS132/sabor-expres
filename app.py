df97dd82bf5e39735a7e9f64f803154139385f38
Davyd Aparecido Zuquim Silva <dzuquim10@gmail.com>
2026-02-16 09:29:22 -0300

    Refatorando Codigo

--- app.py
+++ app.py
@@ -14,33 +14,38 @@ def exibir_menu():
     print('4.Sair\n')
 
 def finalizando_app():
-    os.system('cls')
-    print('Finalizando App\n')
+    exibir_subtitulo('Finalizando App')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu: ')
+    main()
 
 def opcao_invalida():
     print('Opção invalida!\n')
-    input('Digite uma tecla para voltar ao menu: ')
-    main()
+
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes.')
+    exibir_subtitulo('Cadastro de novos restaurantes.')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso.\n')
-    input('\nDigite uma tecla para voltar ao meno: ')
-    main()
+
+    voltar_ao_menu_principal()
 
 def listar_restaurantes():
-    os.system('cls')
-    print('Listar os restaurantes\n')
+    exibir_subtitulo('Listar os restaurantes')
 
     # para cada restaurantes  nalista restaurantes:
     for restaurante in restaurantes:
         print(f'.{restaurante}')
 
-    input('\nDigite uma tecla para voltar ao meno: ')
-    main()
+    voltar_ao_menu_principal()
 
 def exolher_opcao():
     try:
